caa8d1052570a6c87d2884c259a6d451afe44ddc
Lazaro Martinez <139843617+lazwolv@users.noreply.github.com>
2024-09-04 14:29:48 -0700

    learning C++ at CSN

---
+++ CS 135/lab01.cpp
@@ -0,0 +1,19 @@
+/// @file lab01.cpp
+/// @author Lazaro Martinez
+/// @date 09/03/2024
+/// 
+/// @note I pledge my word of honor that I have abided by the
+/// CSN Academic Integrity Policy while completing this assignment.
+/// 
+/// @brief 
+/// 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "My first C++ program." << endl;
+
+    return 0
+}
